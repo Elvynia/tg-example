8a78094472ed35cc8e555e4823f0b1d135e1871c
elvynia <elvynia@gmail.com>
2016-08-19 11:33:45 +0200

    Updated to Trilliangular v0.0.8.

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,7 +1,6 @@
 import {Component, ViewChild} from '@angular/core';
 import {FORM_DIRECTIVES} from '@angular/common';
 import {TrilliangularComponent} from 'trilliangular/app/trilliangular.component';
-import {TrilliangularService} from 'trilliangular/app/trilliangular.service';
 import {TgObjectComponent} from 'trilliangular/core/tg-object.component';
 import {TgRendererComponent} from 'trilliangular/core/tg-renderer.component';
 import {TgCameraComponent} from 'trilliangular/core/tg-camera.component';
@@ -17,7 +16,7 @@ import {ExempleCubeComponent} from './exemple-cube.component';
 	template: `
 		<h1>Trilliangular example</h1>
 		<canvas #renderTarget></canvas>
-		<trilliangular-app width="600" height="400" (initialize)="initialize($event)" (update)="update($event)">
+		<trilliangular width="600" height="400" (initialize)="initialize($event)" (update)="update($event)">
 			<tg-renderer name="WebGLRenderer" [args]="{canvas: renderTarget}"></tg-renderer>
 			<tg-camera name="PerspectiveCamera" [args]="[45, 600 / 400, 1, 1000]"></tg-camera>
 			<tg-scene type="THREE" name="Scene">
@@ -26,7 +25,7 @@ import {ExempleCubeComponent} from './exemple-cube.component';
 					<tg-keyboard keys="l" (keyUp)="switchLight($event)" [global]="globalBind" [scoped]="false"></tg-keyboard>
 				</tg-object>
 			</tg-scene>
-		</trilliangular-app>
+		</trilliangular>
 		<div>
 			Global bind active : <input type="checkbox" [(ngModel)]="globalBind" />
 			<input type="text" />
